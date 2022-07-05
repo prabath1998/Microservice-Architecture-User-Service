a1a977c67f0f5a5b0a2b03854b54e6b44385f3e7
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-05 12:27:32 +0530

    get user details with department

--- src/main/java/com/test/userservice/controller/UserController.java
+++ src/main/java/com/test/userservice/controller/UserController.java
@@ -1,13 +1,28 @@
 package com.test.userservice.controller;
 
+import com.test.userservice.VO.ResponseTemplateVO;
+import com.test.userservice.entity.User;
 import com.test.userservice.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user){
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId){
+        log.info("Inside getUserWithDepartment of UserController");
+        return userService.getUserWithDepartment(userId);
+    }
 }
