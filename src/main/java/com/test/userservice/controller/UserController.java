4df049f1ec35ebfafad2221573a3ab702d63491d
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-05 10:43:38 +0530

    project structure created

---
+++ src/main/java/com/test/userservice/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.test.userservice.controller;
+
+import com.test.userservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+}
---
+++ src/main/java/com/test/userservice/service/UserService.java
@@ -0,0 +1,11 @@
+package com.test.userservice.service;
+
+import com.test.userservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+}
